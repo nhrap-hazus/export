30f743054a3b4a514f1c7fd53eb4d4cdb256b6dd
Raines II <0070642225@FEMA.DHS.GOV>
2020-07-29 14:56:02 -0500

    no update

--- hazus-export-tool.py
+++ hazus-export-tool.py
@@ -1,5 +1,5 @@
-from subprocess import call
 import json
+from subprocess import call
 
 try:
     # load config
@@ -19,13 +19,17 @@ try:
         from src.manage import createHazPyEnvironment
         createHazPyEnvironment()
     else:
-        call('CALL conda.bat activate '+virtual_env+' && start /min python src/app.py', shell=True)
-        call('CALL conda.bat activate '+virtual_env+' && start /min python src/update.py', shell=True)
+        call('CALL conda.bat activate '+virtual_env +
+             ' && start /min python src/app.py', shell=True)
+        # call('CALL conda.bat activate '+virtual_env +
+        #      ' && start /min python src/update.py', shell=True)
 except:
     import ctypes
     import sys
     messageBox = ctypes.windll.user32.MessageBoxW
     try:
-        messageBox(0, u"Unexpected error:" + sys.exc_info()[0] + u" | If this problem persists, contact hazus-support@riskmapcds.com.", u"HazPy", 0x1000)
+        messageBox(0, u"Unexpected error:" + sys.exc_info()
+                   [0] + u" | If this problem persists, contact hazus-support@riskmapcds.com.", u"HazPy", 0x1000)
     except:
-        messageBox(0, u"Unexpected error | If this problem persists, contact hazus-support@riskmapcds.com.", u"HazPy", 0x1000)
+        messageBox(
+            0, u"Unexpected error | If this problem persists, contact hazus-support@riskmapcds.com.", u"HazPy", 0x1000)
